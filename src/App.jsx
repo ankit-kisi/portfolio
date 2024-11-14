b62dea8b12fa505c7e4d49402eb807a178a1186f
Ankit Kisi <kisi7ankit@gmail.com>
2024-11-14 16:05:23 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-4xl text-center ">This is my favourite colour.</p>
+    </div>
+  );
+}
+
+export default App;
